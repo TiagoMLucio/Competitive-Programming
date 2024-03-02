08149e693f35feaec8fa1354bbe6e44f69564563
TiagoMLucio <tiagolucio100@gmail.com>
2024-03-02 14:14:32 -0300

    :art: update: basic template vectors/pairs

--- templates/basic.cpp
+++ templates/basic.cpp
@@ -6,11 +6,7 @@ using ll = long long;
 using ld = long double;
 using str = string;
 
-#define sz(x) static_cast<int>((x).size())
-#define MAXX 1073741823
-#define MAX (1 << 20)
-#define INF 0x3f3f3f3f
-#define MODN 1000000007
+#define sz(x) (int)((x).size())
 #define endl '\n'
 #define int ll
 
@@ -21,21 +17,31 @@ using ii = pair<int, int>;
 
 using iii = tuple<int, int, int>;
 using iiii = tuple<int, int, int, int>;
+#define mt make_tuple
 
-using vi = vector<int>;
-using vii = vector<ii>;
-using viii = vector<iii>;
-using viiii = vector<iiii>;
+template<class T> using V = vector<T>
+using vi = V<int>;
+using vii = V<ii>;
+using viii = V<iii>;
+using viiii = V<iiii>;
+using vb = V<bool>;
+using vs = V<str>;
+using vd = V<double>;
 #define pb push_back
 #define eb emplace_back
 #define all(x) (x).begin(), (x).end()
+#define sor(x) sort(all(x))
 #define allr(x) (x).rbegin(), (x).rend()
+
+const int MODN = (int)1e9+7;
+const int INF = 0x3f3f3f3f;
+const int dx[4]{1,0,-1,0}, dy[4]{0,1,0,-1}; // for every grid problem!!
 
 inline namespace IO {
 #define SFINAE(x, ...)                                                         \
 	template <class, class = void> struct x : std::false_type {};              \
 	template <class T> struct x<T, std::void_t<__VA_ARGS__>> : std::true_type {}
-
+ 
 SFINAE(DefaultI, decltype(std::cin >> std::declval<T &>()));
 SFINAE(DefaultO, decltype(std::cout << std::declval<T &>()));
 SFINAE(IsTuple, typename std::tuple_size<T>::type);
@@ -43,20 +49,20 @@ SFINAE(Iterable, decltype(std::begin(std::declval<T>())));
 
 template<typename T, typename = std::enable_if_t<Iterable<T>::value && !DefaultI<T>::value>>
 std::istream& operator>>(std::istream& is, T& container) {
-	for(auto& item : container)
-		is >> item;
-	return is;
+    for(auto& item : container)
+        is >> item; 
+    return is;
 }
 
 template<typename T1, typename T2>
 std::istream& operator>>(std::istream& is, std::pair<T1, T2>& p) {
-	return is >> p.first >> p.second;
+    return is >> p.first >> p.second;
 }
 
 template<typename... Ts>
 std::istream& operator>>(std::istream& is, std::tuple<Ts...>& tup) {
-	std::apply([&is](auto&... args) { ((is >> args), ...); }, tup);
-	return is;
+    std::apply([&is](auto&... args) { ((is >> args), ...); }, tup);
+    return is;
 }
 
 template <auto &is> struct Reader {
@@ -66,32 +72,32 @@ template <auto &is> struct Reader {
 	}
 	template <class... Ts> void read(Ts &...ts) { ((Impl(ts)), ...); }
 };
-
+ 
 template <class... Ts> void re(Ts &...ts) { Reader<cin>{}.read(ts...); }
-#define def(t, ...)                                                        \
-	t __VA_ARGS__;                                                                    \
-	re(__VA_ARGS__);
-
+#define def(t, args...)                                                        \
+	t args;                                                                    \
+	re(args);
+ 
 template<typename T, typename = std::enable_if_t<Iterable<T>::value && !DefaultO<T>::value>>
 std::ostream& operator<<(std::ostream& os, const T& container) {
-	int i = 0;
+    int i = 0;
 	for (const auto &item : container)
 		os << (i++ ? " " : "") << item;
-	return os;
+    return os;
 }
 
 template<typename T1, typename T2>
 std::ostream& operator<<(std::ostream& os, const std::pair<T1, T2>& p) {
-	return os << p.first << " " << p.second;
+    return os << p.first << " " << p.second;
 }
 
 template<typename... Ts>
 std::ostream& operator<<(std::ostream& os, const std::tuple<Ts...>& tup) {
-	std::apply([&os](const auto&... args) {
-		size_t n = 0;
-		((os << (n++ ? " " : "") << args), ...);
-	}, tup);
-	return os;
+    std::apply([&os](const auto&... args) {
+        size_t n = 0;
+        ((os << (n++ ? " " : "") << args), ...);
+    }, tup);
+    return os;
 }
 
 template <auto &os, bool debug, bool print_nd> struct Writer {
@@ -110,10 +116,10 @@ template <auto &os, bool debug, bool print_nd> struct Writer {
 		} else if constexpr (IsTuple<T>::value) {
 			if (debug) os << '(';
 			std::apply(
-				[this](auto const &...args) {
-					int i = 0;
-					(((i++) ? (os << comma() << " ", Impl(args)) : Impl(args)), ...);
-				}, t);
+			    [this](auto const &...args) {
+				    int i = 0;
+				    (((i++) ? (os << comma() << " ", Impl(args)) : Impl(args)), ...);
+			    }, t);
 			if (debug) os << ')';
 		} else static_assert(IsTuple<T>::value, "No matching type for print");
 	}
@@ -127,12 +133,12 @@ template <auto &os, bool debug, bool print_nd> struct Writer {
 	}
 	template <class F, class... Ts>
 	void print_with_sep(const std::string &sep, F const &f,
-						Ts const &...ts) const {
+	                    Ts const &...ts) const {
 		ImplWrapper(f), ((os << sep, ImplWrapper(ts)), ...), os << '\n';
 	}
 	void print_with_sep(const std::string &) const { os << '\n'; }
 };
-
+ 
 template <class... Ts> void pr(Ts const &...ts) {
 	Writer<cout, false, true>{}.print(ts...);
 }
@@ -140,7 +146,7 @@ template <class... Ts> void ps(Ts const &...ts) {
 	Writer<cout, false, true>{}.print_with_sep(" ", ts...);
 }
 }  // namespace IO
-
+ 
 inline namespace Debug {
 template <typename... Args> void err(Args... args) {
 	Writer<cerr, true, false>{}.print_with_sep(" | ", args...);
@@ -148,45 +154,45 @@ template <typename... Args> void err(Args... args) {
 template <typename... Args> void errn(Args... args) {
 	Writer<cerr, true, true>{}.print_with_sep(" | ", args...);
 }
-
+ 
 void err_prefix(str func, int line, string args) {
 	cerr << "\033[0;31m\u001b[1mDEBUG\033[0m"
-		 << " | "
-		 << "\u001b[34m" << func << "\033[0m"
-		 << ":"
-		 << "\u001b[34m" << line << "\033[0m"
-		 << " - "
-		 << "[" << args << "] = ";
+	     << " | "
+	     << "\u001b[34m" << func << "\033[0m"
+	     << ":"
+	     << "\u001b[34m" << line << "\033[0m"
+	     << " - "
+	     << "[" << args << "] = ";
 }
-
+ 
 #ifdef LOCAL
-#define dbg(...) err_prefix(__FUNCTION__, __LINE__, #__VA_ARGS__), err(__VA_ARGS__)
-#define dbgn(...) err_prefix(__FUNCTION__, __LINE__, #__VA_ARGS__), errn(__VA_ARGS__)
+#define dbg(args...) err_prefix(__FUNCTION__, __LINE__, #args), err(args)
+#define dbgn(args...) err_prefix(__FUNCTION__, __LINE__, #args), errn(args)
 #else
 #define dbg(...)
 #define dbgn(args...)
 #endif
-
+ 
 const auto beg_time = std::chrono::high_resolution_clock::now();
 // https://stackoverflow.com/questions/47980498/accurate-c-c-clock-on-a-multi-core-processor-with-auto-overclock?noredirect=1&lq=1
 double time_elapsed() {
 	return chrono::duration<double>(std::chrono::high_resolution_clock::now() -
-									beg_time)
-		.count();
+	                                beg_time)
+	    .count();
 }
 }  // namespace Debug
 
 inline namespace FileIO {
 void setIn(str s) {
-	if (!freopen(s.c_str(), "r", stdin))
-		fprintf(stderr, "Failed to open input file: %s\n", s.c_str());
+    if (!freopen(s.c_str(), "r", stdin))
+        fprintf(stderr, "Failed to open input file: %s\n", s.c_str());
 }
 void setOut(str s) {
-	if (!freopen(s.c_str(), "w", stdout))
-		fprintf(stderr, "Failed to open output file: %s\n", s.c_str());
+    if (!freopen(s.c_str(), "w", stdout))
+        fprintf(stderr, "Failed to open output file: %s\n", s.c_str());
 }
 void setIO(str s = "") {
-	cin.tie(nullptr)->sync_with_stdio(false);  // unsync C / C++ I/O streams
+	cin.tie(0)->sync_with_stdio(0);  // unsync C / C++ I/O streams
 	cout << fixed << setprecision(12);
 	// cin.exceptions(cin.failbit);
 	// throws exception when do smth illegal
@@ -197,18 +203,19 @@ void setIO(str s = "") {
 
 void solve()
 {
-	def(int,n);
-
-	cout << n << endl;
-
+    def(int,n);
+	
+	
 }
 
 signed main()
 {
-	setIO();
+	setIO();	
 
-	def(int, T);
-	while (T--) solve();
+    def(int, T);
+    while (T--) {
+        solve();
+	}
 
 	// dbg(time_elapsed());
 }
