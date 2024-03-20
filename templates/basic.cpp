db26ec27a77cfc3cb6cd43e945b658feb2737c82
TiagoMLucio <tiagolucio100@gmail.com>
2024-03-20 10:37:12 -0300

    :art: update: basic template

--- templates/basic.cpp
+++ templates/basic.cpp
@@ -6,7 +6,7 @@ using ll = long long;
 using ld = long double;
 using str = string;
 
-#define sz(x) (int)((x).size())
+#define sz(x) static_cast<int>((x).size())
 #define endl '\n'
 #define int ll
 
@@ -19,7 +19,7 @@ using iii = tuple<int, int, int>;
 using iiii = tuple<int, int, int, int>;
 #define mt make_tuple
 
-template<class T> using V = vector<T>
+template<class T> using V = vector<T>;
 using vi = V<int>;
 using vii = V<ii>;
 using viii = V<iii>;
@@ -33,7 +33,7 @@ using vd = V<double>;
 #define sor(x) sort(all(x))
 #define allr(x) (x).rbegin(), (x).rend()
 
-const int MODN = (int)1e9+7;
+const int MODN = static_cast<int>(1e9+7);
 const int INF = 0x3f3f3f3f;
 const int dx[4]{1,0,-1,0}, dy[4]{0,1,0,-1}; // for every grid problem!!
 
@@ -47,58 +47,22 @@ SFINAE(DefaultO, decltype(std::cout << std::declval<T &>()));
 SFINAE(IsTuple, typename std::tuple_size<T>::type);
 SFINAE(Iterable, decltype(std::begin(std::declval<T>())));
 
-template<typename T, typename = std::enable_if_t<Iterable<T>::value && !DefaultI<T>::value>>
-std::istream& operator>>(std::istream& is, T& container) {
-    for(auto& item : container)
-        is >> item; 
-    return is;
-}
-
-template<typename T1, typename T2>
-std::istream& operator>>(std::istream& is, std::pair<T1, T2>& p) {
-    return is >> p.first >> p.second;
-}
-
-template<typename... Ts>
-std::istream& operator>>(std::istream& is, std::tuple<Ts...>& tup) {
-    std::apply([&is](auto&... args) { ((is >> args), ...); }, tup);
-    return is;
-}
-
 template <auto &is> struct Reader {
 	template <class T> void Impl(T &t) {
-		if constexpr (DefaultI<T>::value || Iterable<T>::value || IsTuple<T>::value) is >> t;
-		else static_assert(sizeof(T) == 0, "No matching type for read");
+		if constexpr (DefaultI<T>::value) is >> t;
+		else if constexpr (Iterable<T>::value) {
+			for (auto &x : t) Impl(x);
+		} else if constexpr (IsTuple<T>::value) {
+			std::apply([this](auto &...args) { (Impl(args), ...); }, t);
+		} else static_assert(IsTuple<T>::value, "No matching type for read");
 	}
 	template <class... Ts> void read(Ts &...ts) { ((Impl(ts)), ...); }
 };
  
 template <class... Ts> void re(Ts &...ts) { Reader<cin>{}.read(ts...); }
-#define def(t, args...)                                                        \
-	t args;                                                                    \
-	re(args);
- 
-template<typename T, typename = std::enable_if_t<Iterable<T>::value && !DefaultO<T>::value>>
-std::ostream& operator<<(std::ostream& os, const T& container) {
-    int i = 0;
-	for (const auto &item : container)
-		os << (i++ ? " " : "") << item;
-    return os;
-}
-
-template<typename T1, typename T2>
-std::ostream& operator<<(std::ostream& os, const std::pair<T1, T2>& p) {
-    return os << p.first << " " << p.second;
-}
-
-template<typename... Ts>
-std::ostream& operator<<(std::ostream& os, const std::tuple<Ts...>& tup) {
-    std::apply([&os](const auto&... args) {
-        size_t n = 0;
-        ((os << (n++ ? " " : "") << args), ...);
-    }, tup);
-    return os;
-}
+#define def(t, ...)                                                        \
+	t __VA_ARGS__;                                                                    \
+	re(__VA_ARGS__);
 
 template <auto &os, bool debug, bool print_nd> struct Writer {
 	string comma() const { return debug ? "," : ""; }
@@ -116,10 +80,10 @@ template <auto &os, bool debug, bool print_nd> struct Writer {
 		} else if constexpr (IsTuple<T>::value) {
 			if (debug) os << '(';
 			std::apply(
-			    [this](auto const &...args) {
-				    int i = 0;
-				    (((i++) ? (os << comma() << " ", Impl(args)) : Impl(args)), ...);
-			    }, t);
+				[this](auto const &...args) {
+					int i = 0;
+					(((i++) ? (os << comma() << " ", Impl(args)) : Impl(args)), ...);
+				}, t);
 			if (debug) os << ')';
 		} else static_assert(IsTuple<T>::value, "No matching type for print");
 	}
@@ -133,7 +97,7 @@ template <auto &os, bool debug, bool print_nd> struct Writer {
 	}
 	template <class F, class... Ts>
 	void print_with_sep(const std::string &sep, F const &f,
-	                    Ts const &...ts) const {
+						Ts const &...ts) const {
 		ImplWrapper(f), ((os << sep, ImplWrapper(ts)), ...), os << '\n';
 	}
 	void print_with_sep(const std::string &) const { os << '\n'; }
@@ -157,42 +121,42 @@ template <typename... Args> void errn(Args... args) {
  
 void err_prefix(str func, int line, string args) {
 	cerr << "\033[0;31m\u001b[1mDEBUG\033[0m"
-	     << " | "
-	     << "\u001b[34m" << func << "\033[0m"
-	     << ":"
-	     << "\u001b[34m" << line << "\033[0m"
-	     << " - "
-	     << "[" << args << "] = ";
+		 << " | "
+		 << "\u001b[34m" << func << "\033[0m"
+		 << ":"
+		 << "\u001b[34m" << line << "\033[0m"
+		 << " - "
+		 << "[" << args << "] = ";
 }
  
 #ifdef LOCAL
-#define dbg(args...) err_prefix(__FUNCTION__, __LINE__, #args), err(args)
-#define dbgn(args...) err_prefix(__FUNCTION__, __LINE__, #args), errn(args)
+#define dbg(...) err_prefix(__FUNCTION__, __LINE__, #__VA_ARGS__), err(__VA_ARGS__)
+#define dbgn(...) err_prefix(__FUNCTION__, __LINE__, #__VA_ARGS__), errn(__VA_ARGS__)
 #else
 #define dbg(...)
-#define dbgn(args...)
+#define dbgn(...)
 #endif
  
 const auto beg_time = std::chrono::high_resolution_clock::now();
 // https://stackoverflow.com/questions/47980498/accurate-c-c-clock-on-a-multi-core-processor-with-auto-overclock?noredirect=1&lq=1
 double time_elapsed() {
 	return chrono::duration<double>(std::chrono::high_resolution_clock::now() -
-	                                beg_time)
-	    .count();
+									beg_time)
+		.count();
 }
 }  // namespace Debug
 
 inline namespace FileIO {
 void setIn(str s) {
-    if (!freopen(s.c_str(), "r", stdin))
-        fprintf(stderr, "Failed to open input file: %s\n", s.c_str());
+	if (!freopen(s.c_str(), "r", stdin))
+		fprintf(stderr, "Failed to open input file: %s\n", s.c_str());
 }
 void setOut(str s) {
-    if (!freopen(s.c_str(), "w", stdout))
-        fprintf(stderr, "Failed to open output file: %s\n", s.c_str());
+	if (!freopen(s.c_str(), "w", stdout))
+		fprintf(stderr, "Failed to open output file: %s\n", s.c_str());
 }
 void setIO(str s = "") {
-	cin.tie(0)->sync_with_stdio(0);  // unsync C / C++ I/O streams
+	cin.tie(nullptr)->sync_with_stdio(false);  // unsync C / C++ I/O streams
 	cout << fixed << setprecision(12);
 	// cin.exceptions(cin.failbit);
 	// throws exception when do smth illegal
@@ -203,7 +167,7 @@ void setIO(str s = "") {
 
 void solve()
 {
-    def(int,n);
+	def(int, n);
 	
 	
 }
@@ -212,9 +176,9 @@ signed main()
 {
 	setIO();	
 
-    def(int, T);
-    while (T--) {
-        solve();
+	def(int, T);
+	while (T--) {
+		solve();
 	}
 
 	// dbg(time_elapsed());
